4c76de720f2b921cc36609d73ac6d253c6695894
Cory Stewart <corylstewart@gmail.com>
2014-11-04 14:55:15 -0800

    added setting and getting coords

--- point.py
+++ point.py
@@ -3,5 +3,24 @@ class Point:
         self.x = None
         self.y = None
 
-    def set_x(self):
-        pass
+    def set_x(self, x_coord):
+        self.x = x_coord
+
+    def set_y(self, y_coord):
+        self.y = y_coord
+
+    def set_coord(self, x_coord, y_coord):
+        self.set_x(x_coord)
+        self.set_y(y_coord)
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def get_coord(self):
+        return self.get_x(), self.get_y()
+
+    def point_is_set(self):
+        return self.x and self.b
