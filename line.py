c6d36c6b3acda3c577bf18893aa0faed931c7e59
Cory Stewart <corylstewart@gmail.com>
2014-11-06 14:01:20 -0800

    added some line intersection checks

--- line.py
+++ line.py
@@ -26,23 +26,52 @@ class Line:
     def line_is_set(self):
         return self.point_a.point_is_set() and self.point_b.point_is_set()
 
+    def x_diff(self):
+        return self.point_b.get_x() - self.point_a.get_x()
+
+    def y_diff(self):
+        return self.point_b.get_y() - self.point_a.get_y()
+
     def slope(self):
-        x_diff = self.point_b.get_x() - self.point_a.get_x()
+        x_diff = self.x_diff()
         if x_diff == 0:
             return float('inf')
-        y_diff = self.point_b.get_y() - self.point_a.get_y()
-        return y_diff/x_diff
+        else:
+            y_diff = self.y_diff()
+            return y_diff/x_diff
+
+    def length(self):
+        return (self.x_diff()**2 + self.y_diff()**2)**0.5
 
     def do_lines_intersect(self, other_line):
-        pass
+        if self.lines_are_equivalent(other_line):
+            return True
+        if self.do_lines_share_point(other_line):
+            pass
+        else:
+            pass
+
+    def lines_are_equivalent(self, other_line):
+        return (self.point_a.points_are_equivalent(other_line.point_a) and
+                self.point_b.points_are_equivalent(other_line.point_b)) or \
+               (self.point_a.points_are_equivalent(other_line.point_b) and
+                self.point_b.points_are_equivalent(other_line.point_a))
 
     def do_lines_share_point(self, other_line):
-        pass
+        return self.point_a.points_are_equivalent(other_line.point_a) or \
+            self.point_a.points_are_equivalent(other_line.point_b) or \
+            self.point_b.points_are_equivalent(other_line.point_a) or \
+            self.point_b.points_are_equivalent(other_line.point_a)
 
-    def _does_line_overlap(self, other_line):
+    def do_lines_have_same_slope(self, other_line):
+        return self.slope() == other_line.slop()
+
+    def does_line_overlap(self, other_line):
         pass
 
 
 x = Line()
 x.set_line_by_coord(0, 0, 1, 0)
-print x.slope()
+y = Line()
+y.set_line_by_coord(1, 0, 0, 0)
+print x.lines_are_equivalent(y)
